eefc206170092c693f61a59a3b7562a95a618879
jensee <jech@odoo.com>
2022-03-03 09:57:24 +0530

    add task

---
+++ project_task/__manifest__.py
@@ -0,0 +1,17 @@
+# -- coding: utf-8 --
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    # imfromation
+
+    'name': 'project_submenu',
+    'installable': True,
+    'description': """task id = 2763081""",
+
+    # Dependency
+    'depends': ['base','project'],
+    'data': [
+        'views/project_task_view.xml',
+    ],
+
+}
