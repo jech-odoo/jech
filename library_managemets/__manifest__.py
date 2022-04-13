c59d327c8feed12ad4176b777bd1328e08fdb85b
jensee <jech@odoo.com>
2022-04-13 22:01:09 +0530

    [IMP] a task

--- library_managemets/__manifest__.py
+++ library_managemets/__manifest__.py
@@ -2,16 +2,20 @@
     # imfromation
 
     'name': 'Library Managemets',
-    'installable': True,
 
-    'description': """task id = 2763079""",
+    'description': """ this module is create a library management system.""",
 
+    'author': 'Odoo Ps',
+    'version': '15.0',
     # Dependency
-    # 'depends': ['contacts','sale_management','base','account'],
+   
     'data': [
         'security/ir.model.access.csv',
         'views/library_menu.xml',
         'views/library_view.xml',
     ],
 
+     'installable': True,
+
 }
+
