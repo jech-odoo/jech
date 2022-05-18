4951bcdc334697a5af9eb8d78657ddf245c94fd3
jensee <jech@odoo.com>
2022-05-18 11:34:40 +0530

    [ADD] a task

--- quality_check/__manifest__.py
+++ quality_check/__manifest__.py
@@ -1,15 +1,23 @@
 # -- coding: utf-8 --
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 {
-    # imfromation
-    'name': 'Quality Check',
+    'name': "Quality Check",
+    'summary': """Manage quantity of 'down payment' product in regular invoice""",
+    'description': """ 
+           In This module create a Quality Check, that is linked to a picking, 
+           is moved to the state 'Passed', automatically process the linked picking.
+    """,
+    "author": "Odoo S.A.",
+    "website": "http://www.odoo.com/",
+    "category": "Customizations",
+    "version": "1.0",
+    "license": "LGPL-3",
+    'depends': ['quality_control','sale_management'],
+    
+
+    'demo': [],
+    'application': False,
     'installable': True,
-
-    'description': """This module is create a Quality Check, that is linked to a picking, is moved to the state “Passed”, automatically process the linked picking.""",
-
-    # Dependency
-    'depends': ['quality_control','sale_management','stock'],
-    'data': [
-    ],
-
+    'auto_install': False,
 }
--- quality_check/models/quality_check.py
+++ quality_check/models/quality_check.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from odoo import models, fields, _, api
+from odoo import models
+from odoo.tests import Form
 
 
 class QualityCheck(models.Model):
@@ -11,13 +11,11 @@ class QualityCheck(models.Model):
     def do_pass(self):
         res = super().do_pass()
         if self.picking_id.move_ids_without_package:
-            for move in self.picking_id.move_ids_without_package:
-                if move.product_id==self.product_id:
+            for move in self.picking_id.move_ids_without_package.filtered(lambda x: x.product_id == self.product_id):
                     move.quantity_done = move.forecast_availability
                     picking = self.picking_id.with_context(skip_backorder=False)
-                    picking.button_validate()
+                    action_data = picking.button_validate()
+                    if move.product_uom_qty != move.quantity_done:
+                        backorder_wizard = Form(self.env['stock.backorder.confirmation'].with_context(action_data['context'])).save()
+                        backorder_wizard.process()
         return res
-
-
-
-
