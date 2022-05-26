1eb569d64a13840aa1211b687ce9f33c51cfc564
jensee <jech@odoo.com>
2022-05-26 11:28:09 +0530

    [IMP] delivery_shipping: task

--- delivery_shipping/__manifest__.py
+++ delivery_shipping/__manifest__.py
@@ -1,18 +1,15 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     # imfromation
 
-    'name': 'Delivery Date Shipping ',
+    'name': 'Delivery_Date_Shipping ',
     'installable': True,
-    'description': """task id = 2763078""",
 
     # Dependency
     'depends': ['contacts','product', 'sale_management','stock'],
     'data': [
         'views/res_partner_view.xml',
-        'views/order_sale_view.xml',
-        'views/stock_picking_view.xml',
+        'views/order_sale.xml',
+        'views/stock_picking.xml',
     ],
 
 }
--- delivery_shipping/models/sale_order.py
+++ delivery_shipping/models/sale_order.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-import datetime
+
 from odoo import models,fields,api
+import datetime
 
 class SaleOrder(models.Model):
 
