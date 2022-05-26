7c5a7fea6f904d955e74f8a5835c6e0237908db9
jensee <jech@odoo.com>
2022-05-26 11:25:45 +0530

    [IMP] approve_product: task

--- approve_product/__manifest__.py
+++ approve_product/__manifest__.py
@@ -1,24 +1,13 @@
 {
     # imfromation
 
-    'name': 'Approve Product ',
-   
-
-    'description': """
-         Task id:  2763079
-           This module is to create a approve_product to  approve a product user and administrator.
-    """,
-    'author': 'Odoo Ps',
-    'version': '15.0',
+    'name': 'approve_product ',
+    'installable': True,
 
     # Dependency
     'depends': ['contacts', 'sale_management', 'delivery_barcode', 'stock','base'],
-
     'data': [
-        'views/sale_order.xml',
+        'views/approval_order.xml',
     ],
 
-     'installable': True,
-
 }
-
